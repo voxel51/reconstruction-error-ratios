0b02e5811355b7a95a8bea07a460e9c5845c40c7
Jacob Marks <jamarks13@gmail.com>
2024-12-03 10:19:50 -0500

    update embeddings gen features

--- data_preparation_scripts/generate_embeddings.sh
+++ data_preparation_scripts/generate_embeddings.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 dataset_names=(
-  "OxfordFlowers102" "cifar10" "cifar100" "caltech101" "caltech256" "CUB-200-2011" "deep-weeds" "DescribableTextures" "EuroSAT" "fashion-mnist" "FGVC-Aircraft" "Food101" "MIT-Indoor-Scenes" "mnist" "places" "RESISC45" "StanfordDogs" "SUN397"
+  "OxfordFlowers102" "cifar10" "cifar100" "caltech101" "caltech256" "CUB-200-2011" "deep-weeds" "DescribableTextures" "EuroSAT" "fashion-mnist" "FGVC-Aircraft" "Food101" "MIT-Indoor-Scenes" "mnist" "RESISC45" "StanfordDogs" "SUN397" "places" "imagenet"
 )
 # dataset_names=(
 #  "chestmnist" "dermamnist" "octmnist" "organamnist" "origancmnist" "origansmnist" "pathmnist" "retinamnist" "tissuemnist"
 # )
 
 for dataset_name in "${dataset_names[@]}"; do
-  /home/ubuntu/miniconda3/envs/py_311/bin/python data_preparation_scripts/generate_embeddings.py --dataset_name $dataset_name
+  python data_preparation_scripts/generate_embeddings.py --dataset_name $dataset_name --features "clip-vit-base-patch32"
 done
